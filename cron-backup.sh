18a5afc561791b7b8c7039714fc585a031747f24
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-24 10:18:30 +0100

    daily cron to backup all instances using rsync

---
+++ cron-backup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -xe
+
+# add authorized key on lib10 with
+# command="/srv/gnt-info/gnt-lv-snap-rsync.sh -" ssh-rsa
+
+/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/lib30-id_rsa lib30 | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron.sh
+sh -xe /dev/shm/cron.sh
