6c2e16221b3cd48975db5b0108829b90951ae7ad
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-24 08:58:05 +0100

    add any arguments to grep list

--- zfs-list-backup.sh
+++ zfs-list-backup.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -xe
 
-zfs list -o name,written,compressratio -t snapshot -r lib15/backup
+grep="cat"
+test ! -z "$1" && grep="grep $*"
+
+zfs list -o name,logicalreferenced,written,compressratio -t snapshot -r lib15/backup | $grep
