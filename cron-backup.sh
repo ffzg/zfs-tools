71e2cf36c64554d525cf2a8c5d4e0a19ae810349
Dobrica Pavlinusic <dpavlin@rot13.org>
2016-12-24 16:25:33 +0100

    list backup overview at end

--- cron-backup.sh
+++ cron-backup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -xe
 
-# add authorized key on lib10 with
+# add authorized key on ganeti master node with
 # command="/srv/gnt-info/gnt-lv-snap-rsync.sh -" ssh-rsa
 
 /srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/lib30-id_rsa lib30 | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron.sh
 sh -xe /dev/shm/cron.sh
+/srv/zfs-tools/zfs-list.pl 
