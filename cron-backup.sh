b917434d03309ffbb03453e36e0dd172564f2184
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-06-05 10:01:15 +0200

    run diskrsync from cron

--- cron-backup.sh
+++ cron-backup.sh
@@ -5,4 +5,7 @@
 
 /srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/lib30-id_rsa lib30 | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron.sh
 sh -xe /dev/shm/cron.sh
-/srv/zfs-tools/zfs-list.pl 
+/srv/zfs-tools/zfs-list.pl
+
+/srv/zfs-tools/diskrsync.sh
+
