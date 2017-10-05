a50c6ff28a67a78e5fd1e80acee9570e16463d51
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-10-05 23:15:11 +0200

    added oscar to backup

--- cron-backup.sh
+++ cron-backup.sh
@@ -5,6 +5,10 @@
 
 /srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/lib30-id_rsa lib30 | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron.sh
 sh -xe /dev/shm/cron.sh
+
+export backup=oscar
+/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/r1u28-id_dsa r1u28 | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron-$backup.sh
+
 /srv/zfs-tools/zfs-list.pl
 
 /srv/zfs-tools/diskrsync.sh
