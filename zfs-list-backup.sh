c0ca628b09c3e7afa905f503eaba7beb3b2774c2
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-07-28 11:42:39 +0200

    tee /dev/shm/zfs-list-backup

--- zfs-list-backup.sh
+++ zfs-list-backup.sh
@@ -3,4 +3,4 @@
 grep="cat"
 test ! -z "$1" && grep="grep $*"
 
-sudo zfs list -o name,logicalreferenced,written,compressratio -t snapshot -r lib15/backup lib15/diskrsync lib15/oscar | $grep
+sudo zfs list -o name,logicalreferenced,written,compressratio -t snapshot -r lib15/backup lib15/diskrsync lib15/oscar | $grep | tee /dev/shm/zfs-list-backup
