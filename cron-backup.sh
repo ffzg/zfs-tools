863cea1f20322c4d98f41a24f05c3a50efc55951
Dobrica Pavlinusic <dpavlin@rot13.org>
2018-09-25 12:12:04 +0200

    use ganeti cluster master ip names and new ssh keys

--- cron-backup.sh
+++ cron-backup.sh
@@ -3,11 +3,11 @@
 # add authorized key on ganeti master node with
 # command="/srv/gnt-info/gnt-lv-snap-rsync.sh -" ssh-rsa
 
-/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/lib30-id_rsa lib30 | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron.sh
+/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/lib30-id_rsa root@cluster.gnt.ffzg.hr | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron.sh
 sh -xe /dev/shm/cron.sh
 
 export backup=oscar
-/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/r1u28-id_dsa r1u28 | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron-$backup.sh
+/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/oscar-lv-snap-id_rsa root@oscar.net.ffzg.hr | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron-$backup.sh
 sh -xe /dev/shm/cron-$backup.sh
 
 /srv/zfs-tools/diskrsync.sh
