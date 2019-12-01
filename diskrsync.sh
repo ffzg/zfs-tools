1efbf743de6959b9cd8cd7d65240b102368d767d
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-12-01 11:00:00 +0100

    to work with ganeti masterfailover we need to ignore host key

--- diskrsync.sh
+++ diskrsync.sh
@@ -4,6 +4,9 @@
 
 zpool=`sudo zpool list -H -o name | head -1`
 
+# to work with ganeti masterfailover we need to ignore host key
+ssh='ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
 backup() {
 # broken ident for better readability
 
@@ -14,23 +17,23 @@ disk=$4
 
 zfs list $zpool/diskrsync/$instance || ( echo "ERROR fix with: zfs create -p $zpool/diskrsync/$instance" && exit 1 )
 
-gnt_master=`ssh $cluster_node gnt-cluster getmaster`
+gnt_master=`$ssh $cluster_node gnt-cluster getmaster`
 
 
-instance=`ssh $gnt_master gnt-instance list --no-headers -o name $instance | head -1`
+instance=`$ssh $gnt_master gnt-instance list --no-headers -o name $instance | head -1`
 
-node=`ssh $gnt_master gnt-instance list -o pnode --no-headers $instance`
+node=`$ssh $gnt_master gnt-instance list -o pnode --no-headers $instance`
 echo "# $instance on $node"
 
-ssh $node lvs -o name,tags | grep $instance | tee /dev/shm/$instace.$node.lvs | grep disk${disk}_data | while read lv origin ; do
+$ssh $node lvs -o name,tags | grep $instance | tee /dev/shm/$instace.$node.lvs | grep disk${disk}_data | while read lv origin ; do
 	disk_nr=`echo $lv | cut -d. -f2 | tr -d a-z_`
 	echo "# $lv | $origin | $disk_nr"
 
-	ssh $node lvcreate -L20480m -s -n$lv.snap /dev/$vg/$lv
+	$ssh $node lvcreate -L20480m -s -n$lv.snap /dev/$vg/$lv
 
 	time /usr/local/bin/diskrsync --no-compress --verbose $node:/dev/$vg/$lv.snap /$zpool/diskrsync/$instance/$disk
 
-	ssh $node lvremove -f /dev/$vg/$lv.snap
+	$ssh $node lvremove -f /dev/$vg/$lv.snap
 	date=`date +%Y-%m-%d`
 
 	zfs snap $zpool/diskrsync/$instance@$date
