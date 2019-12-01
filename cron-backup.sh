c2cc2118d76d66e330010230ac921f41353607c8
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-12-01 09:22:09 +0100

    disable host key checks to work with masterfailover

--- cron-backup.sh
+++ cron-backup.sh
@@ -3,18 +3,15 @@
 # add authorized key on ganeti master node with
 # command="/srv/gnt-info/gnt-lv-snap-rsync.sh -" ssh-rsa
 
-/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/lib30-id_rsa root@cluster.gnt.ffzg.hr | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron.sh
+# we need to disable all host key checking since masterfailover from one node to another on ganeti will change this keys
+/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /root/.ssh/lib30-id_rsa root@cluster.gnt.ffzg.hr | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron.sh
 sh -xe /dev/shm/cron.sh
 
 export backup=oscar
-/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -i /root/.ssh/oscar-lv-snap-id_rsa root@oscar.gnt.ffzg.hr | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron-$backup.sh
+/srv/zfs-tools/backup-instances-today.sh | xargs -i echo 'echo {} | ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /root/.ssh/oscar-lv-snap-id_rsa root@oscar.gnt.ffzg.hr | tee "/dev/shm/cron.{}.log"' | tee /dev/shm/cron-$backup.sh
 sh -xe /dev/shm/cron-$backup.sh
 
 /srv/zfs-tools/diskrsync.sh
 
 /srv/zfs-tools/zfs-list.pl | tee /dev/shm/zfs-list.txt
 
-
-ssh lib20 /srv/zfs-tools/diskrsync.sh
-
-ssh lib20 /lib20/zpool-backup/cloud.sh
