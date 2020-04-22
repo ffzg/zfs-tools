e26a3a140fbdffe2d98de425e7be6d9ef86eee1e
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-04-22 09:40:42 +0200

    fix parsing of rsyncd.log

--- rsyncd-log.pl
+++ rsyncd-log.pl
@@ -15,8 +15,10 @@ while(<>) {
 	my @v = split(/\s/,$_);
 	my ( $date, $time, $pid, $op, $from_host, $from_ip, $share, undef, $path, $size );
 	if ( $#v == 9 ) {
+		# 2020/04/22 05:44:30 [5950] recv r1u32.gnt.ffzg.hr [10.80.2.51] oscar () ovpn.ffzg.hr/0/var/log/munin/munin-node.log.3.gz 2534
 		( $date, $time, $pid, $op, $from_host, $from_ip, $share, undef, $path, $size ) = @v;
 	} elsif ( $#v == 6 ) {
+		# 2020/04/21 06:25:03 [18843] recv /lib20/rack2/alfa.ffzg.hr etc/.git/logs/refs/heads/master 6239
 		( $date, $time, $pid, $op, $from_host, $path, $size ) = @v;
 		$from_host =~ s{^.*/}{}; # strip path
 		$path = "$from_host/0/$path"; # host/disk/path
@@ -24,7 +26,13 @@ while(<>) {
 		warn "SKIP $#v: [$_]\n" if $debug;
 		next;
 	}
-	#warn "# $date $pid $op $path $size\n";
+
+	if ( $size !~ m/^\d+$/ ) {
+		warn "SKIP $_\n";
+		next;
+	}
+	warn "# $date $pid $op $path $size\n" if $debug;
+
 	if ( ++$stat->{$op} % 10000 == 0 ) {
 		print STDERR "$op $stat->{$op} ";
 	}
