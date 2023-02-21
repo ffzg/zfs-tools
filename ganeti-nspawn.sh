d31e8fde99693590d5d85cb221bfa8d603d173ec
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-02-21 12:17:35 +0100

    support ens network interfaces and aliases

--- ganeti-nspawn.sh
+++ ganeti-nspawn.sh
@@ -20,17 +20,17 @@ while [ $( brctl show | grep $hostname_if | wc -l ) -gt 1 ] ; do
 	hostname_if=${hostname_if}_
 done
 
-grep link: /zamd/ganeti/*-instances/$instance*  | cut -d: -f3 | cat -n | tee /dev/shm/$instance.br
+grep link: /zamd/ganeti/*-instances/$instance*  | cut -d: -f2 | cat -n | tee /dev/shm/$instance.br
 
 # add bridges
 #cat /dev/shm/deenes.ffzg.hr.br | awk '{ print $2 }' | xargs -i sh -cx 'brctl show {} || brctl addbr {}'
 
 grep -A 3 auto $clone/etc/network/interfaces \
-	| grep -E '(auto eth|bridge_ports)' | cut -d' ' -f2 | cut -d: -f1 | cat -n | tee /dev/shm/$instance.eth
+	| grep -E '(auto eth|auto ens|bridge_ports)' | cut -d' ' -f2 | cut -d: -f1 | uniq | cat -n | tee /dev/shm/$instance.eth
 
 echo -n "systemd-nspawn --directory /$clone --hostname=\"$hostname\" \$@ " > /dev/shm/$instance.nspawn
 
-join /dev/shm/$instance.br /dev/shm/$instance.eth | tee /dev/shm/$instance.nettwork \
+join /dev/shm/$instance.br /dev/shm/$instance.eth | tee /dev/shm/$instance.network \
 	| awk -v hostname=$hostname_if '{ print "--network-veth-extra "hostname"-"$2":"$3 }' | xargs echo >> /dev/shm/$instance.nspawn
 
 chmod 755 /dev/shm/$instance.nspawn
