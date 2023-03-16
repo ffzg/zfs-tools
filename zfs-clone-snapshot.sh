4ab6ffda56bbe4f6ff0da09e827b9d2b3a73fb1e
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-03-16 09:54:49 +0100

    new /cluster /oscar layout

--- zfs-clone-snapshot.sh
+++ zfs-clone-snapshot.sh
@@ -5,7 +5,7 @@ pool=$( zpool list -H -o name )
 
 if [ -z "$snapshot" ]; then
 	#host_disk=`zfs list -H -o name -r lib15/backup | grep '/[0-9]*$' | iselect --all --name="Select host and disk"`
-	host_disk=`zfs list -H -r -o name $( ls -d /$pool/**/{backup,oscar} | sed 's,/,,' ) | grep '/0$' | iselect --all --name="Select host and disk"`
+	host_disk=`zfs list -H -r -o name $( ls -d /$pool/{cluster,oscar} | sed 's,/,,' ) | grep '/0$' | iselect --all --name="Select host and disk"`
 	echo "# reading snapshots for $host_disk"
 	snapshot=`zfs list -o name -r -t snapshot $host_disk | sort -r | iselect --all --name="Select snapshot date to diff"`
 	test -z "$snapshot" && echo "no snapshot selected, aborting" && exit 1
@@ -15,7 +15,7 @@ test -z "$snapshot" && echo "Usage: $0 zfs-snapshot" && exit 1
 
 sudo zfs list -H -t snapshot -o name $snapshot | while read snapshot ; do
 	echo "# clone $snapshot"
-	clone=$( echo $snapshot | sed -e 's,^.*/backup/,,' -e 's,^.*/oscar/,,' -e 's,/\([0-9]*\)@,-\1-,' )
+	clone=$( echo $snapshot | sed -e 's,^.*/cluster/,,' -e 's,^.*/oscar/,,' -e 's,/\([0-9]*\)@,-\1-,' )
 	sudo zfs clone $snapshot $pool/clone/$clone || true
 
 	# prefix hostname with CLONE-
@@ -28,7 +28,8 @@ sudo zfs list -H -t snapshot -o name $snapshot | while read snapshot ; do
 	apt remove -y acpid
 __SHELL__
 	chmod 755 $i_sh
-	echo "EXECUTE in shell # /i.sh"
 
-	systemd-nspawn --directory /$pool/clone/$clone
+	systemd-nspawn --directory /$pool/clone/$clone /i.sh
+
+	echo "## cd /$pool/clone/$clone ; /srv/zfs-tools/ganeti-nspawn.sh --boot"
 done
