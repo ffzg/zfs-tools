2021ce5512f4e01026babd6e364f8f12097ac551
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-03-16 09:56:41 +0100

    fix bridge creation

--- ganeti-bridge.sh
+++ ganeti-bridge.sh
@@ -8,7 +8,7 @@ ip link | grep $grep | grep -- -br | cut -d: -f2 | while read link ; do
 	if=$( echo $link | cut -d@ -f1 )
 	br=$( echo $if | cut -d- -f2 )
 	echo "# $link -> $br $if"
-	brctl show $br | tee /dev/shm/$br || brctl addbr $br
+	brctl show $br || brctl addbr $br
 	grep $if /dev/shm/$br || brctl addif $br $if
 	ip link set $if up
 	ip link set $br up
