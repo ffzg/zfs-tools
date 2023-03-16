d283af6a5acbe506be46e2af99d5e2af21097d98
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-03-16 07:11:51 +0100

    fixed pool name, fix permissions

--- zamd-cron.sh
+++ zamd-cron.sh
@@ -4,14 +4,11 @@ cd /srv/zfs-tools
 
 /srv/zfs-tools/zamd-ganeti-backup.sh
 
+/srv/zfs-tools/fix-permission-denied.sh
+
 /srv/zfs-tools/diskrsync.sh
 
-# rotate logs
-ls /zamd/log/*.log | while read $logfile ; do
-	mv $logfile $logfile-$( date +%Y-%m-%d )
-done
-
-/srv/zfs-tools/zfs-list.pl | tee /dev/shm/zfs-list.txt
+ZFS_POOL=zamd /srv/zfs-tools/zfs-list.pl | tee /dev/shm/zfs-list.txt
 
 SNAPS_KEEP=90 /srv/zfs-tools/zfs-snap-to-dr.pl zamd/oscar/dataverse srce01.net.ffzg.hr srce01
 SNAPS_KEEP=90 /srv/zfs-tools/zfs-snap-to-dr.pl zamd/oscar/koha.ffzg.hr srce01.net.ffzg.hr srce01
