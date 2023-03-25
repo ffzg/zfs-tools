dd06ebfc5aaaa3a35da75cc68e2ec23ac191489e
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-03-25 10:22:38 +0100

    re-try rsync from same snapshot

--- rsync-ganeti.sh
+++ rsync-ganeti.sh
@@ -8,19 +8,35 @@ today=$( date +%Y-%m-%d )
 test -d /zamd/log/rsync/$instance || mkdir -p /zamd/log/rsync/$instance
 
 (
+	:> /zamd/log/rsync/$instance/$today
+
 	ssh root@$cluster.gnt.ffzg.hr /srv/gnt-info/gnt-lv-snap-shell.sh $instance $disk | \
 		tee /dev/shm/snap.$instance.$disk
 
 	rsync_from=$( cat /dev/shm/snap.$instance.$disk| grep rsync-from | cut -d' ' -f2 )
 	rsync_args=$( test -f /zamd/$cluster/$instance/rsync.args && cat /zamd/$cluster/$instance/rsync.args || true )
-	if rsync -raHXAz --numeric-ids --inplace --delete $rsync_args \
-		--log-file=/zamd/log/rsync/$instance/$today \
-		$rsync_from /zamd/$cluster/$instance/$disk/ 2>&1
-		then
-			zfs snap zamd/$cluster/$instance/$disk@$today
-		else
-       			echo $cluster $instance $disk >> /dev/shm/backup.errors
-	fi
+
+	backup_ok=0
+	backup_tries=0
+
+	while [ $backup_ok -eq 0 -a $backup_tries -lt 9 ]
+	do
+		backup_tries=$( expr $backup_tries + 1 )
+		echo "## try: $backup_tries $0 $cluster $instance $disk"
+
+		if rsync -raHXAz --numeric-ids --inplace --delete $rsync_args \
+			--log-file=/zamd/log/rsync/$instance/$today \
+			$rsync_from /zamd/$cluster/$instance/$disk/ 2>&1
+			then
+				zfs snap zamd/$cluster/$instance/$disk@$today
+				backup_ok=1
+			else
+				echo $cluster $instance $disk >> /dev/shm/backup.errors
+				grep rsync: /zamd/log/rsync/$instance/$today | grep 'Permission denied' | sed -e 's/^.*open "//' -e 's/".*$//' | tee /dev/shm/fix-permissions.$instance.files | xargs rm -v
+		fi
+
+	done
+
 	cat /dev/shm/snap.$instance.$disk | grep umount | sh -xe
 2>&1 ) | tee -a /dev/shm/$cluster.log
 
