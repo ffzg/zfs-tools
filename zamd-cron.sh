57f4fa0bc725488199eb9a56ee1bda9a3b153650
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-05-08 17:41:37 +0200

    pull proxmox snapshots

--- zamd-cron.sh
+++ zamd-cron.sh
@@ -9,7 +9,10 @@ cd /srv/zfs-tools
 
 /srv/zfs-tools/diskrsync.sh
 
+./zfs-snap-sync.pl r1u38:zfs zamd/proxmox/r1u38
+
 ZFS_POOL=zamd /srv/zfs-tools/zfs-list.pl | tee /dev/shm/zfs-list.txt
 
 SNAPS_KEEP=90 /srv/zfs-tools/zfs-snap-to-dr.pl zamd/oscar/dataverse srce01.net.ffzg.hr srce01
 SNAPS_KEEP=90 /srv/zfs-tools/zfs-snap-to-dr.pl zamd/oscar/koha.ffzg.hr srce01.net.ffzg.hr srce01
+
