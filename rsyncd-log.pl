6c190a4cb23e27cb90105b686bcd1b55c26d6310
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-08-18 09:46:46 +0200

    cleanup log parser

--- rsyncd-log.pl
+++ rsyncd-log.pl
@@ -14,24 +14,17 @@ my $stat;
 while(<>) {
 	chomp;
 	my @v = split(/\s/,$_);
-	my ( $date, $time, $pid, $op, $from_host, $from_ip, $share, undef, $path, $size, $transfer );
-	   ( $date, $time, $pid ) = @v;
+	# 2023/08/18 03:42:09 [517859] >f+++++++++ var/log/uwsgi/web.ffzg.hr.log-20230818.gz 2451 2410
+	my ( $date, $time, $pid, $op, $path, $size, $transfer ) = @v;
 
 	# 2023/08/15 06:30:37 [914596] "*deleting   home/dpavlin/.cache/mozilla/firefox/pf7sl99z.Default User/cache2/entries/B1C52648EB7985A20F46A8BD48539968FC56323E 0 0"
-	if ( s/"(\S+)\s(.+)\s(\d+)\s(\d+)"// ) {
+	# 2023/08/18 08:28:12 [1293935] ">f.st...... var/log/libvirt/qemu/homeassistant.log 116371 1989493"
+	if ( s/"(\S+)\s(.+)\s(\d+)\s(\d+)"$// ) {
 		( $op, $path, $size, $transfer ) = ( $1,$2,$3,$4 );
-	} elsif ( $#v == 9 ) {
-		# 2020/04/22 05:44:30 [5950] recv r1u32.gnt.ffzg.hr [10.80.2.51] oscar () ovpn.ffzg.hr/0/var/log/munin/munin-node.log.3.gz 2534
-		( $date, $time, $pid, $op, $from_host, $from_ip, $share, undef, $path, $size ) = @v;
 	} elsif ( $#v == 6 ) {
-		# 2020/04/21 06:25:03 [18843] recv /lib20/rack2/alfa.ffzg.hr etc/.git/logs/refs/heads/master 6239
-		( $date, $time, $pid, $op, $from_host, $path, $size ) = @v;
-		$from_host =~ s{^.*/}{}; # strip path
-		$path = "$from_host/0/$path"; # host/disk/path
-	} elsif ( $#v == 7 ) {
-		# 2023/08/15 06:25:09 [914596] ">f+++++++++ entries/00037D6A9CD72D90329CEFD53DCC96CDF5A779C3 10680 10640"
-		( $date, $time, $pid, $op, $path, $size ) = @v;
-warn "XXX ",dump( \@v );
+		# ok
+	} elsif ( 0 && s/(\S+)\s(.+)\s(\d+)\s(\d+)$// ) {
+		( $op, $path, $size, $transfer ) = ( $1,$2,$3,$4 );
 	} else {
 		warn "SKIP $#v: [$_]\n" if $debug;
 		next;
@@ -47,9 +40,7 @@ warn "XXX ",dump( \@v );
 		print STDERR "$op $stat->{$op} ";
 	}
 
-	$size = 1 if ( $op eq 'del.' && $size == 0 ); # count deletes
-
-	$stat->{$date}->{$pid}->{$op} += $size;
+	$stat->{$date}->{$pid}->{$op} += $size if $size;
 
 	my @p = split(/\//, $path);
 
@@ -60,7 +51,6 @@ warn "XXX ",dump( \@v );
 
 	foreach my $to ( 0 .. $to_level ) {
 		my $agg_path = join('/', map { $p[$_] } 0 .. $to );
-		warn "XXX $to $agg_path";
 		$stat->{$date}->{$pid}->{agg}->{ $agg_path }->{$op} += $size;
 	}
 }
