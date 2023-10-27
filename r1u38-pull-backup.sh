9ebb526c158f39c3f76deccc9f9b9b52a2363003
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-10-27 14:06:29 +0200

    pull zfs snapshots from proxmox

---
+++ r1u38-pull-backup.sh
@@ -0,0 +1,10 @@
+
+today=$( date +%Y-%m-%d )
+
+ssh r1u38 zfs list -o name -t volume | grep -f r1u38.backup | xargs -i echo zfs snap {}@$today | tee /dev/shm/r1u38.sh
+scp /dev/shm/r1u38.sh r1u38:/tmp/
+ssh r1u38 sh -x /tmp/r1u38.sh
+
+./zfs-snap-sync.pl r1u38:zfs zamd/proxmox/r1u38
+
+# TODO: expire snapshots on r1u38
