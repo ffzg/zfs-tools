3a0f96a979d0c59a726475600a033cd7208020ae
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-03-19 13:36:33 +0100

    sanoid will create snapshots on source

--- r1u38-pull-backup.sh
+++ r1u38-pull-backup.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
 
 today=$( date +%Y-%m-%d )
 
-ssh r1u38 zfs list -o name -t volume | grep -f r1u38.backup | xargs -i echo zfs snap {}@$today | tee /dev/shm/r1u38.sh
-scp /dev/shm/r1u38.sh r1u38:/tmp/
-ssh r1u38 sh -x /tmp/r1u38.sh
+#ssh r1u38 zfs list -o name -t volume | grep -f r1u38.backup | xargs -i echo zfs snap {}@$today | tee /dev/shm/r1u38.sh
+#scp /dev/shm/r1u38.sh r1u38:/tmp/
+#ssh r1u38 sh -x /tmp/r1u38.sh
 
 ./zfs-snap-sync.pl r1u38:zfs zamd/proxmox/r1u38
 
