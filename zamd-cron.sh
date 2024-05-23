c3e905b7de3967ab3f4aef7654bef98c0bff1539
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-05-23 11:57:34 +0200

    backup rasip to dr site

--- zamd-cron.sh
+++ zamd-cron.sh
@@ -18,4 +18,6 @@ ZFS_POOL=zamd /srv/zfs-tools/zfs-list.pl | tee /dev/shm/zfs-list.txt
 
 SNAPS_KEEP=90 /srv/zfs-tools/zfs-snap-to-dr.pl zamd/oscar/dataverse srce02.net.ffzg.hr srce02
 SNAPS_KEEP=90 /srv/zfs-tools/zfs-snap-to-dr.pl zamd/oscar/koha.ffzg.hr srce02.net.ffzg.hr srce02
+# rasip
+SNAPS_KEEP=90 /srv/zfs-tools/zfs-snap-to-dr.pl zamd/proxmox/r1u38/vm-112-disk-0 srce02.net.ffzg.hr srce02
 
