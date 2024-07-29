05520e56aa9571dd73901a509253b685f8d3061b
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-07-29 11:18:18 +0200

    sort output

--- zfs-diff-www.sh
+++ zfs-diff-www.sh
@@ -6,12 +6,14 @@ to=$3
 
 test -z "$path" -o -z "$from" -o -z "$to" && echo "Usage: $0 zfs/backup/host/3 from-mm-dd toyy-mm-dd" && exit 1
 
-host=`echo $path | cut -d/ -f 3`
+host=`echo $path | cut -d/ -f 3- | tr '/' '.'`
 
-sudo zfs diff -F $path@$from $path@$to | tee /dev/shm/zfs.$host.$from-$to.diff \
+sudo zfs diff -FH $path@$from $path@$to | tee /dev/shm/zfs.$host.$from-$to.diff \
 | grep -v '	/	' \
 | grep -v wp-content/cache \
 | grep -v cache/page \
 | grep -v wp-content/upgrade/wordpress \
 | grep -v wp-content/wflogs \
-| sed "s!/$path/*!!" | tee /dev/shm/zfs.$host.$from-$to.filter
+| grep -v wp-content/upgrade/wordfence \
+| grep -v /dokuwiki/data/cache/ \
+| sed "s!/$path/*!!" | sort -k 2 | tee /dev/shm/zfs.$host.$from-$to.filter
